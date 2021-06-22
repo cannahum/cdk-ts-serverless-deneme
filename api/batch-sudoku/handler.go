dce4716a8b8fd8633c79f14785c31bcf16f3ab93
Can Nahum <cannahum@outlook.com>
2021-06-22 00:12:30 -0700

    SAVEPOINT

--- api/batch-sudoku/handler.go
+++ api/batch-sudoku/handler.go
@@ -11,7 +11,7 @@ type MyEvent struct {
 }
 
 func GenerateBatchSudokuHandler(ctx context.Context, name MyEvent) (string, error) {
-	return "Welcome to GenerateSudokuHandler", nil
+	return "Welcome to GenerateBatchSudokuHandler", nil
 }
 
 func main() {
