e5a4ec359fdd1a7468059ceff9dc7035268a30a1
Can Nahum <cannahum@outlook.com>
2021-06-21 23:01:13 -0700

    WIP

---
+++ lib/sudoku-api-dns.ts
@@ -0,0 +1,13 @@
+// import * as cdk from 'aws-cdk/core';
+
+// interface SudokuAPIDNSProps extends cdk.StackProps {
+//     dnsName: string;
+// }
+
+// class SudokuAPIDNS extends cdk.Stack {
+//   constructor(scope: cdk.Construct, id: string, props: SudokuAPIDNSProps) {
+//     super(scope, id, props);
+//   }
+// }
+
+// export default SudokuAPIDNS;
